629c44bdacdb1cbd24386a95afbc4b5c5dc4b80c
imsiddy <42533510+imsiddy@users.noreply.github.com>
2020-04-01 22:40:47 +0530

    Update connection.dart

--- lib/connection.dart
+++ lib/connection.dart
@@ -6,3 +6,14 @@ class Connection{
 
   Connection(this.id, this.connection_name, this.connection_pin, this.is_high);
 }
+class Changes {
+  final bool is_high;
+
+  Changes({
+    this.is_high,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {"is_high": is_high};
+  }
+}

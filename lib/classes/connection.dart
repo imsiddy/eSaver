80dc4c5321d5241a52472f767c319370711a59c9
imsiddy <42533510+imsiddy@users.noreply.github.com>
2020-04-03 00:23:47 +0530

    Update connection.dart

--- lib/classes/connection.dart
+++ lib/classes/connection.dart
@@ -2,9 +2,11 @@ class Connection{
   final int id;
   final String connection_name;
   final int connection_pin;
+    final String connection_url;
+
   final bool is_high;
 
-  Connection(this.id, this.connection_name, this.connection_pin, this.is_high);
+  Connection(this.id, this.connection_name, this.connection_pin, this.connection_url, this.is_high);
 }
 class Changes {
   final bool is_high;
